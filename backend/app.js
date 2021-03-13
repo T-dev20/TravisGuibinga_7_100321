db0b321740c02b5fb06005b3f4c138ca415d8d36
Travis Gui <maboite41@gmail.com>
2021-03-13 10:41:08 +0100

    création d'un fichier de config de la conn à MySQL

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,8 @@
 const express = require('express');  // Importation de Express
 const bodyParser = require('body-parser');  // Importation de body-parser: analysee les corps de requêtes entrants dans un middleware
 const path = require('path');
-const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host : "localhost",
-    user: "root",
-    password : "Trav15#",
-    database : "groupomania"
-});
-
-//Connexion à MySQL
-connection.connect(function(error) {
-    if (error) {
-        console.error("La connexion à MySQL a échoué !"); 
-        return;
-    }
-    console.log("Connexion à MySQL réussie !");
-});
+var connection = require("./config/mysql.config");
 
 const app = express();
 
---
+++ backend/config/mysql.config.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host : "localhost",
+    user: "root",
+    password : "Trav15#",
+    database : "groupomania"
+});
+
+//Connexion à MySQL
+connection.connect(function(error) {
+    if (error) {
+        console.error("La connexion à MySQL a échoué !"); 
+        return;
+    }
+    console.log("Connexion à MySQL réussie !");
+});
