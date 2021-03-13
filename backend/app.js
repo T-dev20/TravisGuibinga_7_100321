38e5009fbd79979a343bc04a2a431c0c95be4c1e
Travis Gui <maboite41@gmail.com>
2021-03-13 11:02:12 +0100

    import de la route user dans le fichier app.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require('express');  // Importation de Express
 const bodyParser = require('body-parser');  // Importation de body-parser: analysee les corps de requêtes entrants dans un middleware
 const path = require('path');
+const userRoutes = require('./routes/user');
 
 var connection = require("./config/mysql.config");
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/api/auth', userRoutes);
 
 
 module.exports = app;
