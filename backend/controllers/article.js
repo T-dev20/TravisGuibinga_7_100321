995bfacc1650e67b30cfc89d32e7ab380497423b
Travis Gui <maboite41@gmail.com>
2021-03-13 15:11:33 +0100

    fonction copiée-collée : modifiée

--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -21,7 +21,7 @@ exports.createArticle = (req, res, next) => {
     //Recherche dans la BDD de l'article à modifier
     console.log(req.params.id);
     let sql = "SELECT * FROM Article WHERE id = ?";
-    db.query(sql, [req.params.slug], function(err, data) {
+    db.query(sql, [req.params.id], function(err, data) {
         if (err) {
             return res.status(400).json({err});
         }
@@ -38,33 +38,32 @@ exports.createArticle = (req, res, next) => {
                 res.json({status: 201, data, message: "Article modifié !"})
             });
         } else {
-            throw "La modification de l'article est possible soit par son auteur soit par l'Admin.";
+            throw "La modification de l'article n'est possible que soit par son auteur soit par l'Admin.";
         }
     });
  };
 
- exports.modifyArticle = (req, res, next) => {
-    //Recherche dans la BDD de l'article à modifier
+ exports.deleteArticle = (req, res, next) => {
+    //Recherche dans la BDD de l'article à supprimé
     console.log(req.params.id);
-    let sql = "SELECT * FROM Article WHERE id = ?";
-    db.query(sql, [req.params.slug], function(err, data) {
+    let sql = "UPDATE Article SET deleted = true WHERE id = ?";
+    db.query(sql, [req.params.id], function(err, data) {
         if (err) {
             return res.status(400).json({err});
         }
-        var articleToModify = data[0];
+        var articleToDeleted = data[0];
         //Comparaison de l'id du user courant avec l'id du user ayant posté l'article
-        if (articleToModify.user_id === req.user.userId || req.user.isAdmin === 1) {
-            let imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
-            let sql = `UPDATE Article SET content = ?, user_id = ?, imageUrl = ?, date_post = ? WHERE id = ?`;
+        if (articleToDeleted.user_id === req.user.userId || req.user.isAdmin === 1) {
+            let sql = "UPDATE Article SET deleted = true WHERE id = ?";
             let values = [req.body.content, req.body.user_id, imageUrl, req.body.date_post];
             db.query(sql, values, function(err, data, fields) {
                 if (err) {
                     return res.status(400).json({err});
                 }
-                res.json({status: 201, data, message: "Article modifié !"})
+                res.json({status: 201, data, message: "Article supprimé !"})
             });
         } else {
-            throw "La modification de l'article est possible soit par son auteur soit par l'Admin.";
+            throw "La suppression de l'article n'est possible que soit par son auteur soit par l'Admin.";
         }
     });
  };
