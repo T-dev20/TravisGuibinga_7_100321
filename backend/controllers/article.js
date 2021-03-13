2e763438eb52e5fc4626d56a399d11525111b060
Travis Gui <maboite41@gmail.com>
2021-03-13 14:34:42 +0100

    fonction createArticle crée

--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken");
 //Fonction qui gère la logique métier de la route POST (ajout d'un nouvel article)
 exports.createArticle = (req, res, next) => {
     let sql = `INSERT INTO Article(content, user_id, imageUrl, date_post) VALUES (?)`;
+    let imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
     let values = [req.body.content, req.body.user_id, imageUrl, req.body.date_post];
     db.query(sql, [values], function(err, data, fields) {
         if (err) {
