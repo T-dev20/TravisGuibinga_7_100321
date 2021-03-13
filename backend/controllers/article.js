af213d8bef6af26ef33c89aa675cada3a9fb5d67
Travis Gui <maboite41@gmail.com>
2021-03-13 12:01:08 +0100

    fonction createArticle créée

--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -1,3 +1,16 @@
 const Article = require("..models/article");
 var db = require("..config/mysql.config.js");
 const jwt = require("jsonwebtoken");
+
+
+//Fonction qui gère la logique métier de la route POST (ajout d'un nouvel article)
+exports.createArticle = (req, res, next) => {
+    let sql = `INSERT INTO Article(content, user_id, imageUrl, date_post) VALUES (?)`;
+    let values = [req.body.content, req.body.user_id, imageUrl, req.body.date_post];
+    db.query(sql, [values], function(err, data, fields) {
+        if (err) {
+            return res.status(400).json({err});
+        }
+        res.json({status: 201, data, message: "Nouvel article posté avec succès !"})
+    });
+};
