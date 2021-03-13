7f020da9fce877894104cece2e38b653ee78fe73
Travis Gui <maboite41@gmail.com>
2021-03-13 17:35:07 +0100

    multer installé

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');  // Importation de Express
 const bodyParser = require('body-parser');  // Importation de body-parser: analysee les corps de requêtes entrants dans un middleware
 const path = require('path');
 const userRoutes = require('./routes/user');
-const articleRoutes = require('.routes/article');
+const articleRoutes = require('./routes/article');
 
 var connection = require("./config/mysql.config");
 
