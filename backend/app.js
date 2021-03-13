f3b6814c26134cec519a59480f516fd2c1392e39
Travis Gui <maboite41@gmail.com>
2021-03-13 15:24:53 +0100

    route_article : importée dans le fichier app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');  // Importation de Express
 const bodyParser = require('body-parser');  // Importation de body-parser: analysee les corps de requêtes entrants dans un middleware
 const path = require('path');
 const userRoutes = require('./routes/user');
+const articleRoutes = require('.routes/article');
 
 var connection = require("./config/mysql.config");
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/api/auth', userRoutes);
+app.use('/articles', articleRoutes);
 
 
 module.exports = app;
