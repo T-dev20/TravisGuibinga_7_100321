6e7f28c6b91760a67e9035951f7203897a369a86
Travis Gui <maboite41@gmail.com>
2021-03-14 03:09:21 +0100

    routes des posts implémentées

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,15 +1,21 @@
-const express = require("express");
-const router = express.Router(); // On crée un routeur Express
-const auth = require("../middleware/auth");
-const articleCtrl = require("../controllers/article");
+const express = require('express'); // pour importer application Express
+const router = express.Router(); // pour créer un routeur Express
+const auth = require('../middleware/auth'); // pour importer le middleware auth
+const multer = require('../middleware/multer-config'); // pour importer le middleware multer
+const postCtrl = require('../controllers/post'); // pour importer le controleur
 
 
-
-router.post("/add", articleCtrl.createArticle);
-router.get("/", articleCtrl.getAllArticles);
-router.get("/:id", articleCtrl.getOneArticle);
-router.put("/:id", articleCtrl.modifyArticle); 
-router.delete("/:id", articleCtrl.deleteArticle);
- 
+router.get('/', postCtrl.getAllPosts);
+router.get('/:id/comments', postCtrl.getAllComments);
+//pour enregistrer des posts dans la BDD
+router.post('/', auth, multer, postCtrl.createPost);
+//pour afficher un post
+router.get('/:id', auth, postCtrl.getOnePost);
+//pour modifier un post  
+router.put('/:id', auth, multer, postCtrl.modifyPost);
+//pour liker un post
+router.post('/:id/like', auth, postCtrl.likePost)
+//pour supprimer un post 
+router.delete('/:id', auth, postCtrl.deletePost);
 
 module.exports = router;
