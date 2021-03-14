5e57322c793e49a3e2139c18de1da76d56437a3f
Travis Gui <maboite41@gmail.com>
2021-03-14 18:44:35 +0100

    erreur liée au contrôleur post résolue

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,4 +1,4 @@
-const db = require('../models');
+const Comment = require("../models/comment");
 const Post = require("../models/post");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
@@ -8,25 +8,25 @@ exports.createPost = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, "RANDON_SECRET_KEY");
     const userId = decodedToken.userId;
+   
     
-    db.Post.create({
+    Post.create({
         UserId: userId,
         content: req.body.content,
         image: ( req.file ? `${req.protocol}://${req.get('host')}/images/${req.file.filename}` : null )
     })
         .then(post => res.status(201).json({ message: 'Post créé' }))
-        
         .catch(error => res.status(400).json({ message: 'erreur création post' }))
-}
+};
 
 exports.deletePost = (req, res, next) => {
-    db.Post.destroy({ where: { id: req.params.id } })
+    Post.destroy({ where: { id: req.params.id } })
         .then(() => res.status(200).json({ message: 'Post supprimé'}))
         .catch(error => res.status(400).json({ error: 'Problème_suppression_post' }));
 };
 
 exports.getAllPosts = (req, res, next) => {
-    db.Post.findAll({
+    Post.findAll({
         include: {
             model: db.User,
             attributes: ["name", "role", "image"]
@@ -37,16 +37,16 @@ exports.getAllPosts = (req, res, next) => {
     })
         .then(posts => res.status(200).json(posts))
         .catch(error => res.status(500).json({ error }))
-}
+};
 
 exports.getAllComments = (req, res, next) => {
-    db.Comment.findAll({
+    Comment.findAll({
         where: { PostId: req.params.id},
         include: {
-            model: db.User,
+            model: User,
             attributes: ["name", "role", "image"]
         }
     }) 
         .then(comments => res.status(200).json(comments))
         .catch(error => res.status(500).json({ error }))
-}
+};
