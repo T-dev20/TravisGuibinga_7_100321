e0ca1e953acf3b2d4ba11b106ad7b5641b6e3ea0
Travis Gui <maboite41@gmail.com>
2021-03-24 15:25:08 +0100

    Textes d'acceuil modifiés

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <template>
+<div id="app">
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">S'incrire</router-link> |
+    <router-link to="/about">Connexion</router-link>
   </div>
   <router-view/>
+</div>
 </template>
 
 <style lang="scss">
