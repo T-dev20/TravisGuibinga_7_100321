d9f7d5f9ba326fa3bfd34e09f525ca4e167fa6a2
Travis Gui <maboite41@gmail.com>
2021-03-24 16:02:25 +0100

    views import et conn importés

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Connexion from '../views/Connexion.vue'
+import Inscription from '../views/Inscription.vue'
 
 const routes = [
   {
@@ -10,10 +11,7 @@ const routes = [
   {
     path: '/inscription',
     name: 'Inscription',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: Inscription
   }
 ]
 
