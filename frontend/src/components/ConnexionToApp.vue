d74048be9f213d020a529c9bf576b88257127f2a
Travis Gui <maboite41@gmail.com>
2021-03-25 10:06:32 +0100

    composants implémentés

---
+++ frontend/src/components/ConnexionToApp.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">     
+        <div class="col-12">
+            <div class="card cardGroupomania shadow my-4 py-4"> 
+                <div class="card-text">                    
+                    <form class="form-group text-left mx-5">
+                        <h2 class="h4 text-left inline">
+                            Se connecter :                    
+                        </h2>
+                        <div class="font-italic">Pas encore de compte ? <a style="color:#5ef1af" href="http://localhost:8080/#/register">Cliquez ici pour vous inscrire</a></div>
+                        
+                        <br>
+                        <label for="email-inscription">Adresse e-mail</label>
+                        <input id="email-inscription" v-bind:style="border" v-model="email" type="email" placeholder="email@gmail.com" class="form-control" required>  
+                        <br>
+
+                        <label for="mpasse-inscription">Mot de passe</label>
+                        <input id="mpasse-inscription" v-bind:style="border" v-model="password" type="password" placeholder="Motdepasse!1" class="form-control" required>  
+                        <br>
+
+                        <!-- Connexion button -->
+                        <p class="card-text text-center inline">
+                            <button @click="checkForm" id="contact_button-inscription" class="btn font-weight-bold toClick" type="submit"><div class="h4">Se connecter</div></button>
+                        </p>                         
+                    </form>                    
+                </div>
+            </div>
+        </div>             
+    </div>
+</template>
---
+++ frontend/src/components/InscriptionToApp.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="row">     
+        <div class="col-12">
+            <div class="card cardGroupomania shadow my-4 py-4"> 
+                <div class="card-text">                    
+                    <form class="form-group text-left mx-5">
+                        <h2 class="h4 text-left inline">
+                            Première inscription :                    
+                        </h2>
+                        <br>
+
+                        <label for="firstName">Prénom</label>
+                        <input id="firstName" v-bind:style="border" v-model="firstName" type="text" placeholder="Pierre" class="form-control" required>     
+                        <br>
+
+                        <label for="lastName">Nom</label>
+                        <input id="lastName" v-bind:style="border" v-model="lastName" type="text" placeholder="Dupont" class="form-control" required>  
+                        <br>
+
+                        <label for="photo-inscription">Photo de profil <span class="font-italic font-weight-light text-white-50"> Facultatif </span></label>
+                        <p>
+                          <input id="photo-inscription" type="file" @change="imageUpload" class="input">  
+                        </p>
+                        <br>
+
+                        <label for="email-inscription">Adresse e-mail</label>
+                        <input id="email-inscription" v-bind:style="border" v-model="email" type="email" placeholder="email@gmail.com" class="form-control" required>  
+                        <br>
+
+                        <label for="mpasse-inscription">Mot de passe</label>
+                        <p v-if="!this.validPassword(this.password) || !this.password" class="font-italic font-weight-light text-white-50">Doit contenir entre 8 et 15 caractères, des minuscules et majuscules, au moins un chiffre et un caractère spécial </p>
+                        <input id="mpasse-inscription" v-bind:style="border" v-model="password" type="password" placeholder="Motdepasse!1" class="form-control" required>  
+                        <br>
+
+                        <!-- Register button -->
+                        <p class="card-text text-center inline">
+                            <button @click="checkForm" id="contact_button-inscription" class="btn toClick font-weight-bold" type="submit"><div class="h4">S'inscrire</div></button>
+                        </p>                        
+                    </form>
+                </div>
+            </div>
+        </div>             
+    </div>
+</template>
