dc0bbd643e0a763291f80c94d6455d50d1d458b6
Travis Gui <maboite41@gmail.com>
2021-03-25 09:13:18 +0100

    route inscription configurée

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Inscription from '..views/Inscription.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/inscription',
+    name: 'Inscription',
+    component: Inscription,
+    meta : {
+      requiresAuth: false
+    }
+  },
+]
 
 // eslint-disable-next-line no-new
 const router = new VueRouter({
