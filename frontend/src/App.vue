61fe17d47e245f222002c7a3a064974e5242e12d
Travis Gui <maboite41@gmail.com>
2021-03-25 11:07:44 +0100

    version 10 de node-sass installée

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,29 +8,47 @@
   </div>
 </template>
 
-<style lang='scss'>
+<style lang="scss">
+// Definition of Sass variables
+$website-color: #4c8baf;
+$link-color-spec: yellow;
+$separationBar-bgColor:white;
+$likeReaction-color:rgb(47, 255, 47);
+$dislikeReaction-color:rgb(255, 123, 129);
+$cardGroupomania-bgColor: darken($website-color, 20);
+// 
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  // color: #2c3e50;
+  color: white;
+  background: linear-gradient(135deg, $website-color 0%, #c4e0e5 100%);
+  padding-bottom: 2em;
 }
 
 #nav {
   padding: 30px;
+
+  a {
+    font-weight: bold;
+    // color: #2c3e50;
+    color: white;
+
+    &.router-link-exact-active {
+      color: $link-color-spec;
+      // color: #ffdb3c;
+    }
+  }
+
+  .navigationItem {
+    font-weight: bold;
+    // color: #2c3e50;
+    color: white;
+  }
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-// Definition of Sass variables
-$website-color: #4c8baf;
-$cardGroupomania-bgColor: darken($website-color, 20);
 </style>
