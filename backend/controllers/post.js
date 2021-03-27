da2a38fcf7812a4aa7d178af5336ce5a64b55956
Travis Gui <maboite41@gmail.com>
2021-03-27 16:23:32 +0100

    param Postid suppimé

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -18,18 +18,16 @@ exports.createPost = (req, res, next) => {
 };
 
 exports.deletePost = (req, res, next) => {
-    db.Post.destroy({ where: { PostId: req.params.id } })
+    db.Post.destroy({ where: { id: req.params.id } })
         .then(() => res.status(200).json({ message: 'Post supprimé'}))
         .catch(error => res.status(400).json({ error: 'Problème_suppression_post' }));
 };
 
 
 exports.getOnePost = (req, res, next) => {
-  db.Post.findOne({
-    PostId: req.params.id
-  })
+  db.Post.findOne({where: { id: req.params.id }})
       .then(post => res.status(200).json(post))
-      .catch(error => res.status(404).json({error: error}));
+      .catch(error => res.status(404).json({error: 'error'}));
 };
 
 
