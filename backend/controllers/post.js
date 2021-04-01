27cff16fc90efc1034a7b53d0e69c1fa78277931
Travis Gui <maboite41@gmail.com>
2021-04-01 14:32:04 +0200

    fonction getOnePost : .then modifié

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -38,8 +38,8 @@ exports.deletePost = (req, res, next) => {
 
 exports.getOnePost = (req, res, next) => {
   db.Post.findOne({where: { id: req.params.id }})
-      .then(post => res.status(200).json(post))
-      .catch(error => res.status(404).json({error: 'error'}));
+      .then(post => res.status(200).json(post || {} ))
+      .catch(error => res.status(404).json( {} ));
 };
 
 exports.modifyPost = (req, res, next) => {
