d35b4a4815494f2548d8c71a47203bf649b7d8c5
Travis Gui <maboite41@gmail.com>
2021-04-01 11:06:58 +0200

    fonction deleteComment : restriction ajoutée

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -20,7 +20,7 @@ exports.modifyComment = (req, res, next) => {
             .then(() => res.status(201).json({ message: 'Commentaire modifié.' }))
             .catch(error => res.status(400).json({ error }))
             }else {
-                return res.status(404).json({ error: "'Vous n'êtes pas autorisé à modifié ce commentaire !'" })
+                return res.status(404).json({ error: "'Vous n'êtes pas autorisé à modifier ce commentaire !'" })
             }
             
         })
@@ -28,7 +28,21 @@ exports.modifyComment = (req, res, next) => {
 };
 
 exports.deleteComment = (req, res, next) => {
-    db.Comment.destroy({ where: { id: req.params.id } })
-        .then(() => res.status(200).json({ message: 'Commentaire supprimé.'}))
-        .catch(error => res.status(400).json({ error: 'Problème_suppression_commentaire !' }));
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    const userId = decodedToken.userId;
+
+    db.Comment.findOne({ 
+        where: { id: req.params.id } })
+        .then(comment => {
+            if (comment.OwnerId == userId) {
+            comment.destroy({ where: { id: req.params.id } })
+            .then(() => res.status(201).json({ message: 'Commentaire supprimé.' }))
+            .catch(error => res.status(400).json({ error }))
+            }else {
+                return res.status(404).json({ error: "'Vous n'êtes pas autorisé à supprimer ce commentaire !'" })
+            }
+            
+        })
+    .catch(error => res.status(400).json({ error }))
 };
