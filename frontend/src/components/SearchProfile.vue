bec9ff226e2e9f2499cd999675b372a8cd277070
Travis Gui <maboite41@gmail.com>
2021-04-13 15:29:48 +0200

    SearchProfil: template implémenté

---
+++ frontend/src/components/SearchProfile.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="row">     
+    <div class="col-12">
+        <div class="form-inline displayFlexCenter">
+            <img alt="Groupomania logo" src="../assets/logo2.png">
+            <form class="form-group mt-2 mt-sm-0">            
+                <select v-model="idProfileToSee" class="form-control textColorApp">
+                    <option value= null>Consulter un profil...</option>
+                    <option v-for="user in tableUsers" :key="user.id" :value="user.id">{{user.name}} id n°{{user.id}}</option>
+                </select>
+                <button @click="getToProfile" class="btn btn-dark m-1 btnBgApp"> Accéder au profil</button>
+            </form>
+        </div>
+    </div>
+</div>
+</template>
