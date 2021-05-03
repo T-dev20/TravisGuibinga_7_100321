04aa039786c8e157e1941b0ad540774c195febe6
Travis Gui <maboite41@gmail.com>
2021-05-03 15:36:58 +0200

    ajout de balise svg

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -75,6 +75,26 @@
                     <div class="btnAddComment">      
                         <button @click="cacheDisplay('comment-creation'+item.id)" class="btn font-italic toClick" type="submit">Rédiger un commentaire</button>
                     </div>
+                    <a href="./menu-leDeliceDesSens.html">
+                        <div class="restaurants__menus">
+                            <span class="restaurants__menus__heart div-icon"
+                            ><i class="fas fa-thumbs-up"></i>
+                            </span>
+                            <!-- Define svg -->
+                            <svg width="0" height="0">
+                            <linearGradient id="lgrad" x1="0.5" y1="0" x2="0.2" y2="1">
+                                <stop
+                                offset="0%"
+                                style="stop-color: rgb(104, 16, 211); stop-opacity: 1"
+                                />
+                                <stop
+                                offset="75%"
+                                style="stop-color: rgb(189, 28, 143); stop-opacity: 1"
+                                />
+                            </linearGradient>
+                            </svg>
+                        </div>
+                    </a>
                     <span class="likeContainer"> <i class="far fa-thumbs-up"></i> {{item.likes}}</span>
                 </div>
                 <br>
